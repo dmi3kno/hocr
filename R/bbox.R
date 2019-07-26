1d2b0fdbba154254f62c90730221d8d325f9604f
dmi3kno <dperepolkin@gmail.com>
2019-07-27 01:31:11 +0200

    add bbox_pad() and correct bbox_union()/bbox_intersect()

--- R/bbox.R
+++ R/bbox.R
@@ -62,29 +62,59 @@ bbox_slice_xy <- function(bbox, x, y){
 
 #' Functions for aggregating bbox objects
 #' These functions can perform union and intersection operations on bbox objects
-#' @param bbox character vector bounding boxes to perform operation on
+#' @param bbox character vector of bounding boxes to perform operation on
+#' @param bbox2 optional character vector of bounding boxes to element-wise aggregation with `bbox`.
+#' If specified, needs to be length 1 or equal in length to `bbox`.
 #'
 #' @return bbox or missing value, if result is invalid bounding box
 #' @export
 #'
 #' @examples
 #' bbox_union(c("5 1 7 3", "2 4 6 8"))
+#' bbox_union(c("5 1 7 3", "2 4 6 8"), c("1 1 1 1"))
 #' bbox_intersect(c("5 1 7 3", "2 4 6 8")) # should return NA
-#' bbox_intersect(c("5 1 7 3", "2 2 6 8"))
+#' bbox_intersect("5 1 7 3", "2 2 6 8")
 #' @rdname bbox_aggregate
 #'
 
-bbox_union <- function(bbox){
+bbox_union <- function(bbox, bbox2=NULL){
+  if(length(bbox)==1L)
+    bbox <- rep.int(bbox, times=length(bbox2))
+  if(length(bbox2)==1L)
+    bbox2 <- rep.int(bbox2, times=length(bbox))
+  stopifnot(length(bbox)==length(bbox2) || is.null(bbox2))
+
   coord_list <- lapply(strsplit(bbox, ",| "), as.numeric)
   m <- do.call(rbind, coord_list)
+
+  if(!is.null(bbox2)){
+    coord_list2 <- lapply(strsplit(bbox2, ",| "), as.numeric)
+    m2 <- do.call(rbind, coord_list2)
+    return(bbox_validate(paste(pmin(m[,1], m2[,1]), pmin(m[,2], m2[,2]),
+                               pmax(m[,3], m2[,3]), pmax(m[,4], m2[,4]))))
+  }
+
   bbox_validate(paste(min(m[,1]), min(m[,2]), max(m[,3]), max(m[,4])))
 }
 
 #' @rdname bbox_aggregate
 #' @export
-bbox_intersect <- function(bbox){
+bbox_intersect <- function(bbox, bbox2=NULL){
+  if(length(bbox)==1L)
+    bbox <- rep.int(bbox, times=length(bbox2))
+  if(length(bbox2)==1L)
+    bbox2 <- rep.int(bbox2, times=length(bbox))
+
   coord_list <- lapply(strsplit(bbox, ",| "), as.numeric)
   m <- do.call(rbind, coord_list)
+
+  if(!is.null(bbox2)){
+    coord_list2 <- lapply(strsplit(bbox2, ",| "), as.numeric)
+    m2 <- do.call(rbind, coord_list2)
+    return(bbox_validate(paste(pmax(m[,1], m2[,1]), pmax(m[,2], m2[,2]),
+                               pmin(m[,3], m2[,3]), pmin(m[,4], m2[,4]))))
+  }
+
   bbox_validate(paste(max(m[,1]), max(m[,2]), min(m[,3]), min(m[,4])))
 }
 
@@ -112,3 +142,45 @@ bbox_validate <- function(bbox){
   ifelse(bbox_is_valid(bbox), bbox, NA_character_)
 }
 
+#' Functions for padding bbox
+#' These functions can "pad" (increase size of) bbox
+#' @param bbox character vector of bounding boxes to pad
+#' @param word character vector of words contained in bboxes
+#' @param n integer number of symbols to add
+#' @param side "left", "right" or "both" which side to pad
+#'
+#' @return a vector of validated bboxes
+#' @rdname bbox_pad
+#'
+#' @examples
+#' bbox_pad_width("0 0 10 20", "There")
+#' bbox_pad_height("0 0 10 20", "There")
+#' @export
+bbox_pad_width <- function(bbox, word, n=1, side="both"){
+  bb_lst <- lapply(strsplit(bbox, ",| "), as.numeric)
+  m <- do.call(rbind, bb_lst)
+  p <- (m[,3]-m[,1])/nchar(word)
+  if(side=="left"|side=="both")
+    m[,1] <- m[,1]-p*n
+  if(side=="right"|side=="both")
+    m[,3] <- m[,3]+p*n
+
+  bbox_validate(paste(m[,1], m[,2], m[,3],m[,4]))
+}
+
+#' @rdname bbox_pad
+#' @param side "up", "down" or "both" which side to pad
+#' @export
+bbox_pad_height <- function(bbox, word, n=0.5, side="both"){
+
+  bb_lst <- lapply(strsplit(bbox, ",| "), as.numeric)
+  m <- do.call(rbind, bb_lst)
+  n_lines <-  lengths(regmatches(word, gregexpr("\n", word)))+1
+  p <- (m[,4]-m[,2])/n_lines
+  if(side=="up"|side=="both")
+    m[,2] <- m[,2]-p*n
+  if(side=="down"|side=="both")
+    m[,4] <- m[,4]+p*n
+
+  bbox_validate(paste(m[,1], m[,2], m[,3],m[,4]))
+}
